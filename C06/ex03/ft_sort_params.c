dd576c0e8c07de13479d9c50d2f92e8f7556e240
FranciscoLi2000 <liyufeng577@gmail.com>
2026-08-12 14:07:46 +0000

    argc & argv

---
+++ C06/ex03/ft_sort_params.c
@@ -0,0 +1,58 @@
+#include <unistd.h>
+
+static int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] && s2[i] && s1[i] == s2[i])
+		i++;
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+}
+
+static void	ft_sort_args(char **argv, int argc)
+{
+	int		i;
+	int		j;
+	char	*tmp;
+
+	i = 1;
+	while (i < argc - 1)
+	{
+		j = 1;
+		while (j < argc - 1 - (i - 1))
+		{
+			if (ft_strcmp(argv[j], argv[j + 1]) > 0)
+			{
+				tmp = argv[j];
+				argv[j] = argv[j + 1];
+				argv[j + 1] = tmp;
+			}
+			j++;
+		}
+		i++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int	i;
+	int	j;
+
+	if (argc < 2)
+		return (0);
+	ft_sort_args(argv, argc);
+	i = 1;
+	while (i < argc)
+	{
+		j = 0;
+		while (argv[i][j])
+		{
+			write(1, &argv[i][j], 1);
+			j++;
+		}
+		write(1, "\n", 1);
+		i++;
+	}
+	return (0);
+}
